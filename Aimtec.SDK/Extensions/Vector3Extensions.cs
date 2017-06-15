4c9ec3fa9dea2e255d8e274a2eeb47990df88fc7
Exory <enrico.drago3@gmail.com>
2017-06-15 10:39:39 +0200

    Create Vector3Extensions.cs
    Added Vector3,Extend Extensions.

--- Aimtec.SDK/Extensions/Vector3Extensions.cs
+++ Aimtec.SDK/Extensions/Vector3Extensions.cs
@@ -34,5 +34,29 @@ namespace Aimtec.SDK.Extensions
             var allyTurrets = ObjectManager.Get<Obj_AI_Turret>().Any(t => t.IsAlly && point.Distance(t.Position) < 950f + ObjectManager.GetLocalPlayer().BoundingRadius + t.BoundingRadius);
             return allyTurrets;
         }
+        
+        /// <summary>
+        ///     Extends a Vector3 to another Vector3.
+        /// </summary>
+        /// <param name="vector3">the starting position.</param>
+        /// <param name="toVector3">the target direction.</param>
+        /// <param name="distance">the amount of distance.</param>
+        /// <returns>'vector3' extended to 'toVector3' by 'distance'.</returns>
+        public static Vector3 Extend(this Vector3 vector3, Vector3 toVector3, float distance)
+        {
+            return vector3 + distance * (toVector3 - vector3).Normalized();
+        }
+
+        /// <summary>
+        ///     Extends a Vector3 to a Vector2.
+        /// </summary>
+        /// <param name="vector3">the starting position.</param>
+        /// <param name="toVector2">the target direction.</param>
+        /// <param name="distance">the amount of distance.</param>
+        /// <returns>'vector3' extended to 'toVector2' by 'distance'.</returns>
+        public static Vector3 Extend(this Vector3 vector3, Vector2 toVector2, float distance)
+        {
+            return vector3 + distance * ((Vector3)toVector2 - vector3).Normalized();
+        }
     }
 }
