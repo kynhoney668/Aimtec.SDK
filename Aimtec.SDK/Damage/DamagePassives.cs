e53cd1dbae2072bcd3d5628845846649080a96b2
Exory <enrico.drago3@gmail.com>
2017-07-27 06:31:31 +0200

    Aimtec.SDK: 1.0.0.14 (#33)
    * Aimtec.SDK: 1.0.0.14
    
    Damages: Updated damages for 7.15. (And added Kayn)
    DamageReductions: Removed old Urgot's damage reductions.
    DamagePassives: Fixed and Updated Akali for 7.15.
    
    * The return of the savior

--- Aimtec.SDK/Damage/DamagePassives.cs
+++ Aimtec.SDK/Damage/DamagePassives.cs
@@ -48,7 +48,10 @@ namespace Aimtec.SDK.Damage
 
                                          if (source.HasBuff("akalishadowstate"))
                                          {
-                                             return new[] { 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 40, 50, 60, 70, 80, 90, 100 }[source.Level - 1];
+                                             return
+                                                 0.5 * source.FlatPhysicalDamageMod +
+                                                 0.75 * source.TotalAbilityDamage +
+                                                new[] { 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 40, 50, 60, 70, 80, 90, 100 }[source.Level - 1];
                                          }
 
                                          return 0;
