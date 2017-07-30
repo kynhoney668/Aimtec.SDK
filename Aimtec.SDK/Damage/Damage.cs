8b5d42d4b32f4c041ac8f7ee6a9416bd1020bd79
Exory <enrico.drago3@gmail.com>
2017-07-30 15:27:34 +0200

    Land #37, Aimtec.SDK: 1.0.0.17
    Fixed Turret damage to Siege minions and Super minions.

--- Aimtec.SDK/Damage/Damage.cs
+++ Aimtec.SDK/Damage/Damage.cs
@@ -518,7 +518,7 @@ namespace Aimtec.SDK.Damage
             if (source is Obj_AI_Turret)
             {
                 if (minion != null &&
-                    (minion.Name.Contains("Siege") || minion.Name.Contains("Super")))
+                    (minion.UnitSkinName.Contains("Siege") || minion.UnitSkinName.Contains("Super")))
                 {
                     amount *= 0.7;
                 }
