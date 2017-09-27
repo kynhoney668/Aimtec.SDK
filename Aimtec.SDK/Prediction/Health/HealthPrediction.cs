5763c080bb3dcb07104fedef817ebd4ac181532a
Exory <enrico.drago3@gmail.com>
2017-09-27 08:34:38 +0200

    Aimtec.SDK: 1.0.1.31
    Damages: Updated for 7.19.

--- Aimtec.SDK/Prediction/Health/HealthPrediction.cs
+++ Aimtec.SDK/Prediction/Health/HealthPrediction.cs
@@ -27,13 +27,13 @@
         /// <inheritdoc />
         public float GetLaneClearHealthPrediction(Obj_AI_Base target, int time)
         {
-            return Implementation.GetLaneClearHealthPrediction(target, time);
+            return Instance.GetLaneClearHealthPrediction(target, time);
         }
 
         /// <inheritdoc />
         public float GetPrediction(Obj_AI_Base target, int time)
         {
-            return Implementation.GetPrediction(target, time);
+            return Instance.GetPrediction(target, time);
         }
 
         #endregion
