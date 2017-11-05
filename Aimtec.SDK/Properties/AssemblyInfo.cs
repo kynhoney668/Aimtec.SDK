8209207fd2ab2f7f0b6f4c81b6a51f6b0198cc7d
LHS 1140b <LHS1140b@users.noreply.github.com>
2017-11-05 02:21:57 +0100

    Version++

--- Aimtec.SDK/Properties/AssemblyInfo.cs
+++ Aimtec.SDK/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.1.62")]
+[assembly: AssemblyVersion("1.0.1.63")]
 [assembly: AssemblyFileVersion("1.0.0.1")]
