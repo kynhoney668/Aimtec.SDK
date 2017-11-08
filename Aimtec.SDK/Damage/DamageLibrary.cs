9765d3c436d7bce274263291e1c0621c4e14d1a9
Exory <enrico.drago3@gmail.com>
2017-11-08 04:22:14 +0100

    Use 7.22 Damages. (#80)

--- Aimtec.SDK/Damage/DamageLibrary.cs
+++ Aimtec.SDK/Damage/DamageLibrary.cs
@@ -174,7 +174,7 @@ namespace Aimtec.SDK.Damage
             try
             {
                 // TODO: make this load dynamically based on current running game version.
-                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Aimtec.SDK.Damage.Data.7.21.json"))
+                using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Aimtec.SDK.Damage.Data.7.22.json"))
                 {
                     if (stream == null)
                     {
